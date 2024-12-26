b25632307c953d677153931b3758e93a396021a4
Sahil-DigitalOrbit <sahil.singh@digitalorbit.online>
2024-12-26 22:26:33 +0530

    test

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -3,7 +3,7 @@ import ServiceImg1 from '../../assets/images/ServiceImg1.png';
 import ServiceImg2 from '../../assets/images/ServiceImg2.png';
 import ServiceImg3 from '../../assets/images/ServiceImg3.png';
 import ReviewImg from '../../assets/images/ReviewImg.png';
-import { motion, useInView } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { slideAnimation } from '../../config/motion';
 
 function Services() {
